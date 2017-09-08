f12d2b719188e83db5e46f0a972028afd9b7e9d4
fiberwire <andrew.ramos92@gmail.com>
2017-09-08 15:48:18 -0400

    cleaned up, rearranged
    to satisfy linter

--- src/evolution/painting-specimen.ts
+++ src/evolution/painting-specimen.ts
@@ -1,23 +1,23 @@
-import { Specimen, Genome } from 'enome';
-import { Painting } from '../models/painting';
+import { Specimen, Genome } from 'enome'
+import { Painting } from '../models/painting'
 
-import * as _ from 'lodash';
-import { PaintingGenOptions } from './painting-gen-options';
-import { Path } from '../models/path';
+import * as _ from 'lodash'
+import { PaintingGenOptions } from './painting-gen-options'
+import { Path } from '../models/path'
 
 export default class PaintingSpecimen extends Specimen<PaintingGenOptions, Painting> {
 
   public createPhenotype(genotype: Genome<PaintingGenOptions>): Painting {
-    const g = genotype;
-    const o = g.options;
+    const g = genotype
+    const o = g.options
 
     // use a gene to determine how many paths to make
     const paths = _.range(g.g.int(o.minPaths, o.maxPaths))
       .map(() => {
-        return Path.geneticPath(this.genotype);
-      });
+        return Path.geneticPath(this.genotype)
+      })
 
-    return new Painting(o.width, o.height, paths);
+    return new Painting(o.width, o.height, paths)
   }
 
 }
