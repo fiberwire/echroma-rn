d521dde8a8ea15fb7ce79ca922573075d09eb1b5
fiberwire <andrew.ramos92@gmail.com>
2017-09-08 15:19:49 -0400

    init react native app with typescript

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import React from 'react';
+
+interface State {}
+
+interface Props {}
+
+export default class App extends Component<State, Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <Text>Changes you make will automatically reload.</Text>
+                <Text>Shake your phone to open the developer menu.</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
